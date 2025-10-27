b77c0d0c80ccb9293f254c332cf6ef06e6462326
Neverov Andrei <line.yka765@gmail.com>
2025-10-27 07:35:19 +0700

    updated version

--- clspde/version.py
+++ clspde/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.0.1'
-__versiondate__ = '2023-12-04'
-__license__ = f'clspde {__version__} ({__versiondate__}) — © 2023 AN'
+__version__ = '0.0.2'
+__versiondate__ = '2025-10-25'
+__license__ = f'clspde {__version__} ({__versiondate__}) — © 2025 ANever'
