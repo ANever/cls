5905321f60a2a70229a1cd6e79fc3c48f702536a
Neverov Andrei <line.yka765@gmail.com>
2026-05-01 17:14:30 +0300

    outs added

--- tests/run-simplest-sir-mfg.py
+++ tests/run-simplest-sir-mfg.py
@@ -33,14 +33,17 @@ for j in range(k):
         break
 plot(sol)
 
-n = 20
-ts = np.linspace(settings['MODEL']["area_lims"][0, 0], settings['MODEL']["area_lims"][0, 1] - 1e-9, n)
-points = [[t] for t in ts]
-vals = [[sol.eval(np.array([t]), [0], 1)] for t in ts]
 
-out_dict = {'points':points, 'data':vals}
-with open('colloc_solution_I.pkl', 'wb') as out_file:
-    pkl.dump(out_dict, out_file)
+
+n = 20
+for n in 45*np.array([2**i for i in range(10)]):
+    ts = np.linspace(settings['MODEL']["area_lims"][0, 0], settings['MODEL']["area_lims"][0, 1] - 1e-9, n)
+    points = [[t] for t in ts]
+    vals = [[sol.eval(np.array([t]), [0], 1)] for t in ts]
+
+    out_dict = {'points':points, 'data':vals}
+    with open('colloc_solution_I_'+str(n)+'.pkl', 'wb') as out_file:
+        pkl.dump(out_dict, out_file)
 
 out_dict = {'coefs':sol.cells_coefs}
 with open('colloc_solution_coefs.pkl', 'wb') as out_file:
